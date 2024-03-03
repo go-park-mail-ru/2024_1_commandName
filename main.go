2a8b101990beb409cc1a59ba82761bcf9428089b
FunnyRockfish <123645724+FunnyRockfish@users.noreply.github.com>
2024-03-04 01:55:28 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,20 +1,36 @@
 package main
 
-import "ProjectMessenger/auth"
+import (
+	"ProjectMessenger/auth"
+	"github.com/gorilla/mux"
+	"net/http"
+)
 
 import (
 	"ProjectMessenger/models"
 	"fmt"
 )
 
+func main() {
+	Router()
+	users := make([]models.Person, 0)
+	chats := make([]models.Chat, 0)
+	fmt.Println(users, chats)
+}
+
 // @Title Messenger authorization API
 // @Version 1.0
 // @schemes http
 // @host localhost:8080
 // @BasePath  /
-func main() {
-	auth.Start()
-	users := make([]models.Person, 0)
-	chats := make([]models.Chat, 0)
-	fmt.Println(users, chats)
+func Router() {
+	r := mux.NewRouter()
+
+	api := auth.NewMyHandler()
+	r.HandleFunc("/checkAuth", api.CheckAuth)
+	r.HandleFunc("/login", api.Login)
+	r.HandleFunc("/logout", api.Logout)
+	r.HandleFunc("/register", api.Register)
+
+	http.ListenAndServe(":8080", r)
 }
