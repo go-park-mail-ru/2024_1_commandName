278f37d8764dade0c940fdb712b13a80bfed7b68
TestingUser777 <Heidelbeere79@mail.ru>
2024-03-05 12:37:30 +0300

    Add mutex

--- auth/auth.go
+++ auth/auth.go
@@ -8,6 +8,7 @@ import (
 	"math/rand"
 	"net/http"
 	"strings"
+	"sync"
 	"time"
 
 	_ "github.com/swaggo/http-swagger"
@@ -24,6 +25,9 @@ type MyHandler struct {
 	users    map[string]*models.Person
 	chats    map[int]*models.Chat
 	chatUser []*models.ChatUser
+	sessMU   sync.RWMutex
+	usersMU  sync.RWMutex
+	chatsMU  sync.RWMutex
 	isDebug  bool
 }
 
@@ -83,6 +87,9 @@ func NewMyHandler(isDebug bool) *MyHandler {
 		chats:    make(map[int]*models.Chat),
 		chatUser: make([]*models.ChatUser, 0),
 		isDebug:  isDebug,
+		sessMU:   sync.RWMutex{},
+		chatsMU:  sync.RWMutex{},
+		usersMU:  sync.RWMutex{},
 	}
 	handler.fillDB()
 	return handler
@@ -215,8 +222,9 @@ func (api *MyHandler) Logout(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-
+	api.sessMU.Lock()
 	delete(api.sessions, session.Value)
+	api.sessMU.Unlock()
 	session.Expires = time.Now().AddDate(0, 0, -1)
 	http.SetCookie(w, session)
 	err = models.WriteStatusJson(w, 200, nil)
@@ -278,13 +286,16 @@ func (api *MyHandler) Register(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
+	api.usersMU.Lock()
 	_, userFound := api.users[jsonUser.Username]
 	if userFound {
 		err := models.WriteStatusJson(w, 400, models.Error{Error: "user already exists"})
 		if err != nil {
 			http.Error(w, "internal server error", 500)
+			api.usersMU.Unlock()
 			return
 		}
+		api.usersMU.Unlock()
 		return
 	}
 	jsonUser.ID = uint(len(api.users) + 1)
@@ -293,9 +304,11 @@ func (api *MyHandler) Register(w http.ResponseWriter, r *http.Request) {
 	jsonUser.PasswordSalt = passwordSalt
 
 	api.users[jsonUser.Username] = &jsonUser
+	api.usersMU.Unlock()
 	sessionID := randStringRunes(32)
-
+	api.sessMU.Lock()
 	api.sessions[sessionID] = &jsonUser
+	api.sessMU.Unlock()
 
 	cookie := &http.Cookie{
 		Name:    "session_id",
@@ -416,6 +429,7 @@ func (api *MyHandler) GetChats(w http.ResponseWriter, r *http.Request) {
 
 func (api *MyHandler) getChatsByID(userID uint) []*models.Chat {
 	userChats := make(map[int]*models.Chat)
+	api.chatsMU.Lock()
 	for _, cUser := range api.chatUser {
 		if cUser.UserID == userID {
 			chat, ok := api.chats[cUser.ChatID]
@@ -424,6 +438,7 @@ func (api *MyHandler) getChatsByID(userID uint) []*models.Chat {
 			}
 		}
 	}
+	api.chatsMU.Unlock()
 
 	var chats []*models.Chat
 	for _, chat := range userChats {
