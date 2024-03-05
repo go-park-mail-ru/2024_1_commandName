eaa69531a6e7d3098453b52913595e014cf90ae3
TestingUser777 <Heidelbeere79@mail.ru>
2024-03-05 23:19:23 +0300

    Add new messages, new user (testUser), fixed some bugs in chats

--- auth/auth.go
+++ auth/auth.go
@@ -58,7 +58,7 @@ func setDebugHeaders(w http.ResponseWriter, r *http.Request) (needToReturn bool)
 	header.Add("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
 	header.Add("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
 	header.Add("Access-Control-Allow-Credentials", "true")
-  
+
 	if r.Method == "OPTIONS" {
 		w.WriteHeader(http.StatusOK)
 		needToReturn = true
@@ -72,7 +72,7 @@ func NewMyHandler(isDebug bool) *MyHandler {
 	handler := &MyHandler{
 		sessions: make(map[string]*models.Person, 10),
 		users: map[string]*models.Person{
-			"admin": {ID: 1, Username: "admin", Email: "admin@mail.ru", Name: "Ivan", Surname: "Naumov",
+			"IvanNaumov": {ID: 1, Username: "IvanNaumov", Email: "ivan@mail.ru", Name: "Ivan", Surname: "Naumov",
 				About: "Frontend Developer", CreateDate: time.Now(), LastSeenDate: time.Now(), Avatar: "avatarPath",
 				PasswordSalt: adminSalt, Password: adminHash},
 			"ArtemkaChernikov": {ID: 2, Username: "ArtemkaChernikov", Email: "artem@mail.ru", Name: "Artem", Surname: "Chernikov",
@@ -84,7 +84,10 @@ func NewMyHandler(isDebug bool) *MyHandler {
 			"AlexanderVolohov": {ID: 4, Username: "AlexanderVolohov", Email: "Volohov@mail.ru", Name: "Alexander", Surname: "Volohov",
 				About: "Frontend Developer", CreateDate: time.Now(), LastSeenDate: time.Now(), Avatar: "avatarPath",
 				PasswordSalt: adminSalt, Password: adminHash},
-			"mentor": {ID: 4, Username: "mentor", Email: "mentor@mail.ru", Name: "Mentor", Surname: "Mentor",
+			"mentor": {ID: 5, Username: "mentor", Email: "mentor@mail.ru", Name: "Mentor", Surname: "Mentor",
+				About: "Developer", CreateDate: time.Now(), LastSeenDate: time.Now(), Avatar: "avatarPath",
+				PasswordSalt: adminSalt, Password: adminHash},
+			"testUser": {ID: 6, Username: "TestUser", Email: "test@mail.ru", Name: "Test", Surname: "User",
 				About: "Developer", CreateDate: time.Now(), LastSeenDate: time.Now(), Avatar: "avatarPath",
 				PasswordSalt: adminSalt, Password: adminHash},
 		},
@@ -371,27 +374,58 @@ func (api *MyHandler) ClearUserData() {
 }
 
 func (api *MyHandler) fillDB() {
+	api.chatUser = append(api.chatUser, &models.ChatUser{ChatID: 1, UserID: 6})
+	api.chatUser = append(api.chatUser, &models.ChatUser{ChatID: 1, UserID: 5})
+	api.chatUser = append(api.chatUser, &models.ChatUser{ChatID: 2, UserID: 6})
+	api.chatUser = append(api.chatUser, &models.ChatUser{ChatID: 2, UserID: 2})
+	api.chatUser = append(api.chatUser, &models.ChatUser{ChatID: 3, UserID: 6})
+	api.chatUser = append(api.chatUser, &models.ChatUser{ChatID: 3, UserID: 3})
+	api.chatUser = append(api.chatUser, &models.ChatUser{ChatID: 4, UserID: 6})
+	api.chatUser = append(api.chatUser, &models.ChatUser{ChatID: 4, UserID: 1})
+	api.chatUser = append(api.chatUser, &models.ChatUser{ChatID: 5, UserID: 6})
+	api.chatUser = append(api.chatUser, &models.ChatUser{ChatID: 5, UserID: 4})
+	/////////////////////////////////////////////////////////
+
 	messagesChat1 := make([]*models.Message, 0)
 	messagesChat1 = append(messagesChat1,
 		&models.Message{ID: 1, ChatID: 1, UserID: api.users["mentor"].ID, Message: "Очень хороший код, ставлю 100 баллов", Edited: false},
-		//&models.Message{ID: 2, ChatID: 1, UserID: api.users["admin1"].ID, Message: "Балдёж балдёж", Edited: false},
 	)
-	chat1 := models.Chat{Name: "noName", ID: 1, Type: "person", Description: "", AvatarPath: "", CreatorID: "1", Messages: messagesChat1}
+
+	chat1 := models.Chat{Name: "mentor", ID: 1, Type: "person", Description: "", AvatarPath: "", CreatorID: "1", Messages: messagesChat1, Users: api.getChatUsersByChatID(1)}
 	api.chats[chat1.ID] = &chat1
+	/////////////////////////////////////////////////////////////
 
 	messagesChat2 := make([]*models.Message, 0)
 	messagesChat2 = append(messagesChat2,
 		&models.Message{ID: 1, ChatID: 2, UserID: api.users["ArtemkaChernikov"].ID, Message: "Пойдём в столовку?", Edited: false},
-		//&models.Message{ID: 2, ChatID: 2, UserID: api.users["admin3"].ID, Message: "Уже бегу", Edited: false},
 	)
-	chat2 := models.Chat{Name: "noName", ID: 2, Type: "person", Description: "", AvatarPath: "", CreatorID: "3", Messages: messagesChat2}
+	chat2 := models.Chat{Name: "ArtemkaChernikov", ID: 2, Type: "person", Description: "", AvatarPath: "", CreatorID: "2", Messages: messagesChat2, Users: api.getChatUsersByChatID(2)}
 	api.chats[chat2.ID] = &chat2
+	////////////////////////////////////////////////////////////
 
-	api.chatUser = append(api.chatUser, &models.ChatUser{ChatID: 1, UserID: 1})
-	api.chatUser = append(api.chatUser, &models.ChatUser{ChatID: 1, UserID: 5})
-	api.chatUser = append(api.chatUser, &models.ChatUser{ChatID: 1, UserID: 2})
-	api.chatUser = append(api.chatUser, &models.ChatUser{ChatID: 2, UserID: 3})
-	api.chatUser = append(api.chatUser, &models.ChatUser{ChatID: 2, UserID: 4})
+	messagesChat3 := make([]*models.Message, 0)
+	messagesChat3 = append(messagesChat3,
+		&models.Message{ID: 1, ChatID: 3, UserID: api.users["ArtemZhuk"].ID, Message: "Ты пр уже создал? А то пора уже с мейном мерджить", Edited: false},
+	)
+	chat3 := models.Chat{Name: "ArtemZhuk", ID: 3, Type: "person", Description: "", AvatarPath: "", CreatorID: "3", Messages: messagesChat3, Users: api.getChatUsersByChatID(3)}
+	api.chats[chat3.ID] = &chat3
+	////////////////////////////////////////////////////////////
+
+	messagesChat4 := make([]*models.Message, 0)
+	messagesChat4 = append(messagesChat4,
+		&models.Message{ID: 1, ChatID: 4, UserID: api.users["IvanNaumov"].ID, Message: "Ты когда тесты и авторизацию допилишь, собака сутулая?", Edited: false},
+	)
+	chat4 := models.Chat{Name: "IvanNaumov", ID: 4, Type: "person", Description: "", AvatarPath: "", CreatorID: "1", Messages: messagesChat4, Users: api.getChatUsersByChatID(4)}
+	api.chats[chat4.ID] = &chat4
+	//////////////////////////////////////////////////////////////
+
+	messagesChat5 := make([]*models.Message, 0)
+	messagesChat5 = append(messagesChat5,
+		&models.Message{ID: 1, ChatID: 5, UserID: api.users["AlexanderVolohov"].ID, Message: "Фронт уже готов, когда бек доделаете??", Edited: false},
+	)
+	chat5 := models.Chat{Name: "AlexanderVolohov", ID: 5, Type: "person", Description: "", AvatarPath: "", CreatorID: "5", Messages: messagesChat5, Users: api.getChatUsersByChatID(5)}
+	api.chats[chat5.ID] = &chat5
+	///////////////////////////////////////////////////////////////
 
 }
 
@@ -459,4 +493,14 @@ func (api *MyHandler) getChatsByID(userID uint) []*models.Chat {
 		chats = append(chats, chat)
 	}
 	return chats
+}
+
+func (api *MyHandler) getChatUsersByChatID(chatID int) []*models.ChatUser {
+	usersOfChat := make([]*models.ChatUser, 0)
+	for i := range api.chatUser {
+		if api.chatUser[i].ChatID == chatID {
+			usersOfChat = append(usersOfChat, api.chatUser[i])
+		}
+	}
+	return usersOfChat
 }
--- main.go
+++ main.go
@@ -36,8 +36,9 @@ func Router() {
 	r.HandleFunc("/logout", api.Logout)
 	r.HandleFunc("/register", api.Register)
 	r.HandleFunc("/getChats", api.GetChats)
-	err := http.ListenAndServe(":8080", r)
+	err := http.ListenAndServe(":8082", r)
 	if err != nil {
+		fmt.Println("err")
 		log.Fatal(err)
 		return
 	}
