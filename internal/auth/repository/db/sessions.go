ae406bc416d93fd0df76e46345366a8cb018d8ab
TestingUser777 <Heidelbeere79@mail.ru>
2024-03-28 00:15:13 +0300

    Fix a very important bug

--- internal/auth/repository/db/sessions.go
+++ internal/auth/repository/db/sessions.go
@@ -15,7 +15,7 @@ type Sessions struct {
 }
 
 func (s *Sessions) GetUserIDbySessionID(ctx context.Context, sessionID string) (userID uint, sessionExists bool) {
-	err := s.db.QueryRowContext(ctx, "SELECT id FROM auth.session WHERE sessionid = $1", sessionID).Scan(&userID)
+	err := s.db.QueryRowContext(ctx, "SELECT userid FROM auth.session WHERE sessionid = $1", sessionID).Scan(&userID)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return 0, false
