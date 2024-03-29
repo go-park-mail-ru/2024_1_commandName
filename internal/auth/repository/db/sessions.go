963493477bc2e33b05e5ce86e82180acddaf6a30
Artem Zhuk <artem.zx656@gmail.com>
2024-03-30 02:31:39 +0300

    added log for users.go

--- internal/auth/repository/db/sessions.go
+++ internal/auth/repository/db/sessions.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"log/slog"
 
 	"ProjectMessenger/domain"
 	"ProjectMessenger/internal/misc"
@@ -15,7 +16,9 @@ type Sessions struct {
 }
 
 func (s *Sessions) GetUserIDbySessionID(ctx context.Context, sessionID string) (userID uint, sessionExists bool) {
+	logger := slog.With("requestID", ctx.Value("traceID"))
 	err := s.db.QueryRowContext(ctx, "SELECT userid FROM auth.session WHERE sessionid = $1", sessionID).Scan(&userID)
+	logger.Debug("GetUserIDbySessionID", "userID", userID, "sessionID", sessionID)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return 0, false
@@ -25,14 +28,17 @@ func (s *Sessions) GetUserIDbySessionID(ctx context.Context, sessionID string) (
 			Message: err.Error(),
 			Segment: "method GetUserIDbySessionID, sessions.go",
 		}
-		fmt.Println(customErr.Error())
+		//fmt.Println(customErr.Error())
+		logger.Error(customErr.Error())
 		return 0, false
 	}
 	return userID, true
 }
 
 func (s *Sessions) CreateSession(ctx context.Context, userID uint) (sessionID string) {
-	fmt.Println("create session for user", userID)
+	logger := slog.With("requestID", ctx.Value("traceID"))
+	logger.Debug("CreateSession", "userID", userID)
+	//fmt.Println("create session for user", userID)
 	sessionID = misc.RandStringRunes(32)
 	_, err := s.db.ExecContext(ctx, "INSERT INTO auth.session (sessionid, userid) VALUES ($1, $2)", sessionID, userID)
 	if err != nil {
@@ -41,13 +47,16 @@ func (s *Sessions) CreateSession(ctx context.Context, userID uint) (sessionID st
 			Message: err.Error(),
 			Segment: "method CreateSession, sessions.go",
 		}
-		fmt.Println(customErr.Error())
+		//fmt.Println(customErr.Error())
+		logger.Error(customErr.Error())
 		return ""
 	}
 	return sessionID
 }
 
 func (s *Sessions) DeleteSession(ctx context.Context, sessionID string) {
+	logger := slog.With("requestID", ctx.Value("traceID"))
+	logger.Debug("DeleteSession", "sessionID", sessionID)
 	_, err := s.db.ExecContext(ctx, "DELETE FROM auth.session WHERE sessionID = $1", sessionID)
 	if err != nil {
 		customErr := &domain.CustomError{
@@ -60,5 +69,6 @@ func (s *Sessions) DeleteSession(ctx context.Context, sessionID string) {
 }
 
 func NewSessionStorage(db *sql.DB) *Sessions {
+	slog.Info("created session storage")
 	return &Sessions{db: db}
 }
