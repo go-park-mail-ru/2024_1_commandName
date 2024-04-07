bf36613a8e8c719d13fb92328d010ffc26e31f15
Artem Zhuk <artem.zx656@gmail.com>
2024-04-07 15:41:40 +0300

    added config

--- cmd/messenger/main.go
+++ cmd/messenger/main.go
@@ -4,9 +4,11 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	_ "github.com/swaggo/echo-swagger/example/docs"
+	"gopkg.in/yaml.v3"
 
 	authdelivery "ProjectMessenger/internal/auth/delivery"
 	chatsdelivery "ProjectMessenger/internal/chats/delivery"
@@ -17,14 +19,39 @@ import (
 	database "ProjectMessenger/db"
 )
 
-var DEBUG = false
-var INMEMORY = false
-var AVATAR_PATH = "./uploads/"
-
 func main() {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	slog.SetDefault(logger)
-	Router()
+	cfg := loadConfig()
+	Router(cfg)
+}
+
+type config struct {
+	Server struct {
+		Host string `yaml:"host"`
+		Port int    `yaml:"port"`
+	} `yaml:"server"`
+	App struct {
+		IsDebug    bool   `yaml:"isDebug"`
+		InMemory   bool   `yaml:"inMemory"`
+		AvatarPath string `yaml:"avatarPath"`
+	} `yaml:"app"`
+}
+
+func loadConfig() config {
+	f, err := os.Open("config.yml")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	var cfg config
+	decoder := yaml.NewDecoder(f)
+	err = decoder.Decode(&cfg)
+	if err != nil {
+		panic(err)
+	}
+	return cfg
 }
 
 // swag init -d cmd/messenger/,domain/,internal/
@@ -35,7 +62,7 @@ func main() {
 // @schemes http
 // @host localhost:8080
 // @BasePath  /
-func Router() {
+func Router(cfg config) {
 	router := mux.NewRouter()
 
 	var authHandler *authdelivery.AuthHandler
@@ -43,12 +70,12 @@ func Router() {
 	var profileHandler *profiledelivery.ProfileHandler
 	var messageHandler *messagedelivery.MessageHandler
 
-	if INMEMORY {
+	if cfg.App.InMemory {
 		authHandler = authdelivery.NewAuthMemoryStorage()
 		//chatsHandler = chatsdelivery.NewChatsHandlerMemory(authHandler)
 	} else {
 		dataBase := database.СreateDatabase()
-		authHandler = authdelivery.NewAuthHandler(dataBase, AVATAR_PATH)
+		authHandler = authdelivery.NewAuthHandler(dataBase, cfg.App.AvatarPath)
 		chatsHandler = chatsdelivery.NewChatsHandler(authHandler, dataBase)
 		messageHandler = messagedelivery.NewMessagesHandler(authHandler, dataBase)
 	}
@@ -75,13 +102,13 @@ func Router() {
 	router.HandleFunc("/getChatMessages", messageHandler.GetChatMessages)
 
 	// middleware
-	if DEBUG {
+	if cfg.App.IsDebug {
 		router.Use(middleware.CORS)
 	}
 	router.Use(middleware.AccessLogMiddleware)
 
-	slog.Info("http server starting on 8080")
-	err := http.ListenAndServe(":8080", router)
+	slog.Info("http server starting on " + strconv.Itoa(cfg.Server.Port))
+	err := http.ListenAndServe(cfg.Server.Host+":"+strconv.Itoa(cfg.Server.Port), router)
 	if err != nil {
 		slog.Error("server failed with ", "error", err)
 		return
