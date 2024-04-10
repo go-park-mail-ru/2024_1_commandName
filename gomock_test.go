e73f30fdc42a9ac22b3a1f19328782fc31e18ce7
TestingUser777 <Heidelbeere79@mail.ru>
2024-04-10 17:56:51 +0300

    Add GoMock

--- gomock_test.go
+++ gomock_test.go
@@ -1,11 +1,14 @@
 package _024_1_commandName
 
 import (
+	"context"
 	"database/sql"
+	"fmt"
 	"testing"
 	"time"
 
 	"ProjectMessenger/internal/auth/delivery"
+	database "ProjectMessenger/internal/auth/repository/db"
 	"github.com/DATA-DOG/go-sqlmock"
 )
 
@@ -116,4 +119,46 @@ func TestUserRepo(t *testing.T) {
 			t.Errorf("there were unfulfilled expectations: %s", err)
 			return
 		}*/
+}
+
+func TestGetByUserID(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("cant create mock: %s", err)
+	}
+	defer db.Close()
+
+	// Создаем объект userRepo с mock-объектом базы данных
+	userRepo := database.NewUserStorage(db, "")
+
+	// Устанавливаем ожидание запроса к базе данных на выборку данных пользователя по его ID
+	mock.ExpectQuery("SELECT id, username, email, name, surname, about, password_hash, create_date, lastseen_datetime, avatar, password_salt FROM auth.person WHERE id = ?").
+		WithArgs(6).
+		WillReturnRows(sqlmock.NewRows([]string{"id", "username", "email", "name", "surname", "about", "password_hash", "create_date", "lastseen_datetime", "avatar", "password_salt"}).
+			AddRow(6, "TestUser", "test@mail.ru", "Test", "User", "Developer", "5baae85b9413d75de29d9e54b0550eae8ea8eaabb80b0cea8974bb5ee844b82fd9c45d188938bbc57716a495a3766b1728bdffb04f256a67ad545b62d9e69ac7", time.Now(), time.Now(), "", "gxYdyp8Z"))
+
+	// Создаем контекст
+	ctx := context.Background()
+
+	// Вызываем метод GetByUserID
+	user, found := userRepo.GetByUserID(ctx, 6)
+	fmt.Println(found)
+	// Проверяем, что пользователь был найден
+	if !found {
+		t.Error("expected user to be found, but it was not found")
+	}
+
+	// Проверяем данные пользователя
+	if user.ID != 6 {
+		t.Errorf("expected user ID to be 6, got %d", user.ID)
+	}
+	if user.Username != "TestUser" {
+		t.Errorf("expected username to be test_username, got %s", user.Username)
+	}
+	// Проверяем остальные поля пользователя аналогичным образом
+
+	// Проверяем, что все ожидаемые запросы были выполнены
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
 }
