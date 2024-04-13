99c7506a060dc720d1b77f23d6d21faa7100ba62
TestingUser777 <Heidelbeere79@mail.ru>
2024-04-13 09:52:27 +0300

    Add initial migration

--- gomock_test.go
+++ gomock_test.go
@@ -1,12 +1,17 @@
 package _024_1_commandName
 
 import (
+	"bytes"
 	"context"
 	"database/sql"
 	"fmt"
+	"io/ioutil"
+	"mime/multipart"
+	"os"
 	"testing"
 	"time"
 
+	"ProjectMessenger/domain"
 	//"ProjectMessenger/domain"
 	"ProjectMessenger/internal/auth/delivery"
 	database "ProjectMessenger/internal/auth/repository/db"
@@ -92,31 +97,107 @@ func TestUsers(t *testing.T) {
 	}
 
 	//test #2
-	/*
-		mock.ExpectQuery("SELECT id, username, email, name, surname, about, password_hash, create_date, lastseen_datetime, avatar, password_salt FROM auth.person WHERE id = ?").
-			WithArgs(6).
-			WillReturnRows(sqlmock.NewRows([]string{"id", "username", "email", "name", "surname", "about", "password_hash", "create_date", "lastseen_datetime", "avatar", "password_salt"}).
-				AddRow(6, "TestUser", "test@mail.ru", "Test", "User", "Developer", "5baae85b9413d75de29d9e54b0550eae8ea8eaabb80b0cea8974bb5ee844b82fd9c45d188938bbc57716a495a3766b1728bdffb04f256a67ad545b62d9e69ac7", time.Now(), time.Now(), "", "gxYdyp8Z"))
+	ctx = context.Background()
+	updatedUser := &domain.Person{
+		ID:           6,
+		Username:     "New TestUser",
+		Email:        "test@mail.ru",
+		Name:         "Test",
+		Surname:      "User",
+		About:        "Developer",
+		Password:     "5baae85b9413d75de29d9e54b0550eae8ea8eaabb80b0cea8974bb5ee844b82fd9c45d188938bbc57716a495a3766b1728bdffb04f256a67ad545b62d9e69ac7",
+		CreateDate:   time.Now(),
+		LastSeenDate: time.Now(),
+		Avatar:       "New Avatar",
+		PasswordSalt: "gxYdyp8Z",
+	}
 
-		ctx = context.Background()
-		user = &domain.Person{
-			ID:       8,
-			Username: "New username",
-		}
-		found := userRepo.UpdateUser(ctx, user)
-		if !found {
-			t.Error("expected user to be found, but it was not found")
-		}
-		if user.ID != 6 {
-			t.Errorf("expected user ID to be 6, got %d", user.ID)
-		}
-		if user.Username != "TestUser" {
-			t.Errorf("expected username to be test_username, got %s", user.Username)
-		}
-		if err := mock.ExpectationsWereMet(); err != nil {
-			t.Errorf("there were unfulfilled expectations: %s", err)
-		}*/
+	found = userRepo.UpdateUser(ctx, *updatedUser)
+	if !found {
+		t.Error("expected user to be updated, but it was not found")
+	}
+	if user.ID != 6 {
+		t.Errorf("expected user ID to be 6, got %d", user.ID)
+	}
+	if user.Username != "TestUser" {
+		t.Errorf("expected username to be test_username, got %s", user.Username)
+	}
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+
+	//test #3
+	ctx = context.Background()
+
+	file, err := os.Open("image.jpg")
+	if err != nil {
+		t.Fatalf("failed to open file: %v", err)
+	}
+	defer file.Close()
+
+	// Создаем multipart.FileHeader
+	fileHeader := &multipart.FileHeader{
+		Filename: "image.jpg",
+		Size:     123, // Замените это значение на реальный размер файла
+	}
+
+	// Создаем буфер для чтения содержимого файла
+	buf, err := ioutil.ReadAll(file)
+	if err != nil {
+		t.Fatalf("failed to read file: %v", err)
+	}
+
+	// Создаем multipart.File из буфера
+	multipartFile := &ReadSeekCloser{bytes.NewReader(buf.Bytes())}
+	name, err := userRepo.StoreAvatar(ctx, multipartFile, fileHeader)
+
+	if name != "TestUser" {
+		t.Error("expected username is TestUser, but come another - ", name)
+	}
+	if err != nil {
+		t.Errorf("error:", err)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+
+	//test #4
+	ctx = context.Background()
+	updatedUser := &domain.Person{
+		ID:           6,
+		Username:     "New TestUser",
+		Email:        "test@mail.ru",
+		Name:         "Test",
+		Surname:      "User",
+		About:        "Developer",
+		Password:     "5baae85b9413d75de29d9e54b0550eae8ea8eaabb80b0cea8974bb5ee844b82fd9c45d188938bbc57716a495a3766b1728bdffb04f256a67ad545b62d9e69ac7",
+		CreateDate:   time.Now(),
+		LastSeenDate: time.Now(),
+		Avatar:       "New Avatar",
+		PasswordSalt: "gxYdyp8Z",
+	}
+
+	found = userRepo.UpdateUser(ctx, *updatedUser)
+	if !found {
+		t.Error("expected user to be updated, but it was not found")
+	}
+	if user.ID != 6 {
+		t.Errorf("expected user ID to be 6, got %d", user.ID)
+	}
+	if user.Username != "TestUser" {
+		t.Errorf("expected username to be test_username, got %s", user.Username)
+	}
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
 }
+
+type ReadSeekCloser struct {
+	*bytes.Reader
+}
+
+func (r *ReadSeekCloser) Close() error { return nil }
 
 func TestUpdateUser(t *testing.T) {
 	db, mock, err := sqlmock.New()
